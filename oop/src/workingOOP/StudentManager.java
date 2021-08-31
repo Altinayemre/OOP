036aca8c887667894bfc9b2394527fc58f733820
Emre Altınay <81089561+Altinayemre@users.noreply.github.com>
2021-08-31 21:37:36 +0300

    Update StudentManager.java

--- oop/src/workingOOP/StudentManager.java
+++ oop/src/workingOOP/StudentManager.java
@@ -2,7 +2,7 @@ package workingOOP;
 
 public class StudentManager extends UserManager{
 	public void courseRegister(Student student, Course course) {
-		System.out.println(student.getFullName()+": "+course.courseName+" kursuna kayýt oldu.");
+		System.out.println(student.getFullName()+": "+course.courseName+" kursuna kayit oldu.");
 	}
 	
 	public void courseEvaluation(Student student,String message) {
